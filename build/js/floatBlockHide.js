550a3b4830fd0b5d5ade58448dd3258211f34d23
mazax1ct <vova@giveandget.ru>
2020-12-10 22:24:57 +0300

    fix float-bar toggler on input focus

--- build/js/floatBlockHide.js
+++ build/js/floatBlockHide.js
@@ -1,7 +1,7 @@
 $(document).on('focus', 'input, textarea', function () {
-  $('.float-bar').hide();
+  $('.float-bar').addClass('hidden');
 });
 
 $(document).on('blur', 'input, textarea', function () {
-  $('.float-bar').show();
+  $('.float-bar').removeClass('hidden');
 });
