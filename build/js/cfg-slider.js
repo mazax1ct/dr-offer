63c50c0eda3db1402859af6a5a14f602ea87a660
mazax1ct <vova@giveandget.ru>
2020-12-28 15:07:18 +0300

    msi selection

---
+++ build/js/cfg-slider.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  if($('body').width() < 1200) {
+    //слайдер конфигураций
+    if($('.js-cfg-slider').length) {
+      $('.js-cfg-slider').on('init', function(event, slick) {
+        var list = $('.js-cfg-slider').find('.slick-dots');
+        $.each(list['0'].children, function(index, value) {
+          if(index < 9) {
+            list['0'].children[index].children['0'].innerText = '0' + list['0'].children[index].children['0'].innerText;
+          }
+        });
+      });
+
+      $('.js-cfg-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          }
+        ]
+      });
+    }
+  }
+});
---
+++ build/js/info-popover.js
@@ -0,0 +1,4 @@
+$(document).on('click', '.js-info', function () {
+  $(this).toggleClass('is-active');
+  return false;
+});
